ec91804e523ea7a3676eeb21da3a39f926de8bdf
Sipon Hossain <sipon.here@gmail.com>
2018-01-22 03:39:25 +1000

    commit

---
+++ PhoneWord/PhoneWord.iOS/PhoneDialer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using PhoneWord.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace PhoneWord.iOS
+{
+    class PhoneDialer:IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
---
+++ PhoneWord/PhoneWord/IDialer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneWord
+{
+   public interface IDialer
+    {
+        bool Dial(string number);
+    }
+}
